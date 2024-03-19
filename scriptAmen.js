e3ca331cf96dfcb0090cd6493dc2be59411ab51d
dashagenze <89853583+dashagenze@users.noreply.github.com>
2024-03-19 16:59:20 +0300

    Update scriptAmen.js

--- scriptAmen.js
+++ scriptAmen.js
@@ -1,5 +1,8 @@
 const USERNAME_FORM = document.getElementById('USERNAME_FORM');
 const body = document.getElementById('body');
+let title = document.createElement('h2');
+let div = document.getElementById('div');
+div.append(title);
 
 USERNAME_FORM.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -12,45 +15,55 @@ USERNAME_FORM.addEventListener('submit', (e) => {
     } catch {
         alert(e);
     }
-
-
+    
     USERNAME_FORM.style.display = 'none';
 })
 
 
 async function getRepos (username) {
     await fetch(`https://api.github.com/users/${username}/repos`)
+        .catch(e => {
+            console.log(e)
+
+        })
         .then(r => r.json())
+        .catch(e => {
+            console.log(e)
+
+        })
         .then(repos => {
-
-            let title = document.createElement('h2');
             title.textContent = `📑Репозитории ${username}!`;
-
-            let div = document.getElementById('div');
-            div.append(title);
 
             // ОШИБКА НЕТ РЕПОЗИТОРИЕВ
             if (repos.length === 0) {
                 title.textContent = `У пользователя ${username} не найдено открытых репозиториев :)`
             }
-            for (let repo of repos) {
-                let repoLink = document.createElement('a');
-                repoLink.setAttribute('href', `${repo.url}`);
-                let divider = document.createElement('hr');
-                repoLink.innerHTML = `📁💫${repo.name}`;
 
-                div.append(repoLink);
-                div.append(divider);
+            try {
+                for (let repo of repos) {
+                    let repoLink = document.createElement('a');
+                    repoLink.setAttribute('href', `${repo.url}`);
+                    let divider = document.createElement('hr');
+                    repoLink.innerHTML = `📁💫${repo.name}`;
 
-                console.log(repo.name);
-                console.log(repo.url);
+                    div.append(repoLink);
+                    div.append(divider);
+
+                    console.log(repo.name);
+                    console.log(repo.url);
+                }
+            } catch (e) {
+                if (e instanceof TypeError) {
+                    alert(`такого пользователя нет`)
+                    location.reload()
+                }
             }
 
         })
+
+
         .catch(e => {
-            console.log(e.message)
-                // alert('пользователь не найден ;(((');
-                // location.reload()
+            console.log(e)
             }
         )
 
